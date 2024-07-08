77d03946cd22c31a79b8a21aaf007f2b88d70fc9
tko <tattey.subscriptions@gmail.com>
2024-07-08 15:05:01 +0300

    style: eslint fix

--- src/modules/brain-calc/generate-calc-expression.js
+++ src/modules/brain-calc/generate-calc-expression.js
@@ -1,12 +1,11 @@
 import getRandomNumber from '../brain-even/get-random-number.js';
 
 export default () => {
-
-  let operationIdx = getRandomNumber(0, 3);
+  const operationIdx = getRandomNumber(0, 3);
   let num1 = getRandomNumber(1, 100);
   let num2 = getRandomNumber(1, 100);
 
-  //make sure num1 bigger in subtraction
+  // make sure num1 bigger in subtraction
   if (operationIdx === 1 && num2 > num1) {
     [num1, num2] = [num2, num1];
   }
