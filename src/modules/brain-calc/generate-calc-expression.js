331dc05508aebe7a1a57262b9fe8e74560b93d52
tko <tattey.subscriptions@gmail.com>
2024-07-08 15:03:23 +0300

    fix: make sure subtraction result is never negative in brain-calc game

--- src/modules/brain-calc/generate-calc-expression.js
+++ src/modules/brain-calc/generate-calc-expression.js
@@ -1,7 +1,15 @@
 import getRandomNumber from '../brain-even/get-random-number.js';
 
-export default () => [
-  getRandomNumber(0, 3),
-  getRandomNumber(1, 100),
-  getRandomNumber(1, 100),
-];
+export default () => {
+
+  let operationIdx = getRandomNumber(0, 3);
+  let num1 = getRandomNumber(1, 100);
+  let num2 = getRandomNumber(1, 100);
+
+  //make sure num1 bigger in subtraction
+  if (operationIdx === 1 && num2 > num1) {
+    [num1, num2] = [num2, num1];
+  }
+
+  return [operationIdx, num1, num2];
+};
