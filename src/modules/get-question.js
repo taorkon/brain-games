319e616154d1c671f2dd2903ffa7054878c5b4cf
tko <tattey.subscriptions@gmail.com>
2024-07-08 16:11:28 +0300

    style: eslint fix

--- src/modules/get-question.js
+++ src/modules/get-question.js
@@ -2,19 +2,19 @@ import getEvenQuestion from './games/brain-even/get-even-question.js';
 import getCalcQuestion from './games/brain-calc/get-calc-question.js';
 
 export default (game) => {
-    let question; let
-        correctAnswer;
+  let question; let
+    correctAnswer;
 
-    switch (game) {
-        case 'brain-even':
-            [question, correctAnswer] = getEvenQuestion();
-            break;
-        case 'brain-calc':
-            [question, correctAnswer] = getCalcQuestion();
-            break;
-        default:
-            break;
-    }
+  switch (game) {
+    case 'brain-even':
+      [question, correctAnswer] = getEvenQuestion();
+      break;
+    case 'brain-calc':
+      [question, correctAnswer] = getCalcQuestion();
+      break;
+    default:
+      break;
+  }
 
-    return [question, correctAnswer];
+  return [question, correctAnswer];
 };
