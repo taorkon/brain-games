e45f9a7d5ed544d58f2cd6257e0e4b63e5096be3
tko <tattey.subscriptions@gmail.com>
2024-07-08 16:52:50 +0300

    build: renamed module in brain calc for clarity

--- src/modules/games/brain-calc/get-calc-question.js
+++ src/modules/games/brain-calc/get-calc-question.js
@@ -1,10 +1,10 @@
 import generateCalcExpression from './generate-calc-expression.js';
 import countCalcExpression from './count-calc-expression.js';
-import getStringExpression from './get-string-expression.js';
+import getCalcStringExpression from './get-calc-string-expression.js';
 
 export default () => {
   const generatedExpression = generateCalcExpression();
-  const question = getStringExpression(...generatedExpression);
+  const question = getCalcStringExpression(...generatedExpression);
   const correctAnswer = countCalcExpression(...generatedExpression);
 
   return [question, correctAnswer];
