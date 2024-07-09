718fe9c2c65e0a3c8760ce0f30bbcd45ff7a88f7
tko <tattey.subscriptions@gmail.com>
2024-07-09 13:07:34 +0300

    style: eslint fix

--- src/modules/games/brain-even/get-even-question.js
+++ src/modules/games/brain-even/get-even-question.js
@@ -3,7 +3,7 @@ import isEven from './is-even.js';
 
 export default () => {
   const question = getRandomNumber(1, 100);
-  const correctAnswer = isEven(question) ? "yes" : "no";
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
 
   return [question, correctAnswer];
 };
